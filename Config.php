5c9792ca4be3828594ac8b853c7f8029745c9621
Lorxiic <48385637+Lorxiic@users.noreply.github.com>
2023-03-10 14:20:39 +0700

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php 
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "b9b5e8da62dfb5";
+$db_pass = "54eed56e";
+$db_name = "heroku_65b9cc0294fb8bd";
+
+$connect = mysqli_connect($db_host,$db_user,db_pass,$db_name) or die("database connection error");
